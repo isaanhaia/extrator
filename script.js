2ea5eac9d276bce4b15f825d2456e39ca3aa71e3
Isabela <anhaia.isabela@escola.pr.gov.br>
2025-08-08 13:49:22 +0000

    aula 3

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const BotaoMostraPalavras = document.querySelector('#botao-palavrachave');
 BotaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave (){
-    alert('Fui Clicado! ');
+const texto = document.querySelector('#entrada-de-texto').value; 
+const campoResultado = document.querySelector ('#resultado-palavrachave');
+const palavras = texto.split(" ");
+campoResultado.textContent = palavras.join(", ");
 }
